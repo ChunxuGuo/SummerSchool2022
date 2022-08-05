b203b529ac5cbc099e559dc7aca72eed4d9cf450
Chunxu Guo <chunxug@amd.com>
2022-08-05 19:15:09 +0800

    add lab2

---
+++ Lab2_Sobel/src/sobel.h
@@ -0,0 +1,17 @@
+#include <hls_stream.h>
+#include "ap_axi_sdata.h"
+#include <stdint.h>
+#include "ap_int.h"
+
+#ifndef SOBEL_H_
+#define SOBEL_H_
+
+#define WIDTH 	1280
+#define HEIGHT	720
+
+typedef unsigned char PIXEL;
+typedef ap_axiu<8, 0, 0, 0> trans_pkt;
+
+void sobel(hls::stream<trans_pkt>& src, hls::stream<trans_pkt>& dst, int rows, int cols);
+
+#endif
